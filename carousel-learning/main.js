765dff93740383303413e40d028eca5893a94b9d
unknown <m.mazouz67@gmail.com>
2022-02-08 13:48:31 +0100

    finishing carousel

--- carousel-learning/main.js
+++ carousel-learning/main.js
@@ -1,20 +1,27 @@
 import './style.css'
 
-function getImg(n){
-    let children = Array.from(document.getElementById("app").children)
-    return children[n]
+const changePlace = function(oldClass, newClass, anime){
+    let element = document.getElementsByClassName(oldClass)[0]
+    element.style.animation=`${anime} 1000ms`
+    setTimeout(()=>{
+        element.classList.remove(oldClass)
+        element.classList.add(newClass)
+    },1000)
+    
 }
 
+let currentImg = 0
 
-let currentImg = 2
 document.getElementById("change").onclick = ()=>{
-    getImg(currentImg).style.animation="active-to-after 1000ms "
-    setTimeout(()=>{
-        getImg(currentImg).classList.remove("active")
-    },1000)
-    currentImg--
-    getImg(currentImg).style.animation="before-to-active 1000ms"
-    setTimeout(()=>{
-        getImg(currentImg).classList.add("active")
-    },1000)
+
+    let indicators = document.getElementsByClassName("circle")
+    indicators[currentImg].classList.remove("current")
+    currentImg == 4 ? currentImg = 0 : currentImg++
+    indicators[currentImg].classList.add("current")
+    changePlace("first", "before", "first-to-before")
+    changePlace("active","after", "active-to-after")
+    changePlace("before","active", "before-to-active")
+    changePlace("after", "last", "after-to-last")
+    changePlace("last", "first", "last-to-first")
+    
 }
