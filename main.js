8622c9209aa9005742023ff53bc8d4b74ff5b86c
unknown <m.mazouz67@gmail.com>
2022-02-13 21:10:12 +0100

    responsive's becoming boring

--- main.js
+++ main.js
@@ -1,2 +1,10 @@
 import './style.css'
 
+let langDrop = document.getElementById("langage__drop")
+let langList = document.getElementById("langage__list")
+langDrop.onmouseover = ()=>{
+    langList.style.height = "150px"
+}
+langList.onmouseleave = ()=>{
+    langList.style.height = "0"
+}
